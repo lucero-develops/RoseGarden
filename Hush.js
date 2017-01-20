922d626931010ee4f436ddfdfc877799353eb958
lucero-develops <mike.j.lucero@gmail.com>
2017-01-20 18:24:37 -0500

    my first for loop

--- Hush.js
+++ Hush.js
@@ -1,31 +1,27 @@
-var a = 10;
+/*
+for ( intitialization expression ; conditional expression; update clause)
+*/
 
-if (a > 5) {
-	a = a * 2;
+for ( a =5; a < 10 ; a = a + 1){
+	console.log(a);
+	
 }
+/*this for loop prints 
+5
+6
+7
+8
+9
+*/
 
-//there are curtain expression
-//that would not result in true or false by themselves
-// if(a)  , the if statement is expecting a true or false
-//if not it has to convert 
+/* this for loop will start a at the value 5 
+and each time the loop finished its going 
+to update a by 1 and if a is less than 10 its going to
+do the loop again and again */
 
-/*falsy:
-0 -0 NaN 
-""
-falsy
-null
-undefined
 
-so memorize this list and if its on the falsy list its 
-truthy 
+for (;;){}
 
-example */
-
-var b  = 0;
-
-if (b) {
-	b = b * 3;
-}
-
-/* b is falsy because its in the falsy list , so our 
-if statment reads "if false" not "if true"
+/* this loop will run forever but it doesnt run forever because
+the conditional is always true , its runs forever because there is 
+no conditional that fails to stop it running */
