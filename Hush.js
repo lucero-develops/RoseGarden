c10397eef1bc7c23095b0dfb5d902ba44348c937
lucero-develops <mike.j.lucero@gmail.com>
2017-01-19 21:29:20 -0500

    c over p

--- Hush.js
+++ Hush.js
@@ -1,7 +1,2 @@
-//even if you know all the rules of 
-//operator presidence , you don't always 
-// want to write a program that relise on one.
-//use () to help identify operator presidence
-
-
-
+think of java script more of a compiled language
+then an interpreted language
