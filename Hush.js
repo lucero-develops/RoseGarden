67bf1cc8fc09eb6cafbf9a14e9268f8db21badd9
lucero-develops <mike.j.lucero@gmail.com>
2017-01-20 17:15:22 -0500

    looks the same , but is not . types matter

--- Hush.js
+++ Hush.js
@@ -1,13 +1,15 @@
-a = a + 2
-a+=2
-//both have same result
+//number
+42;
 
-// += 
-//just adds something to my 
-//existing variable
+//string
+//what we do input and output with
+"42" [0]; // "4";
 
-// once you know this short cut 
-//it conceptulally simpler
-//to understand the statement 
+// the value look the same , but 
+//they behave differently in JS
+//becauuse they are of different types 
 
-a += 2
+// using quotes expresses your going to use
+//the value in a string way ,  they can 
+//be single or double quotes , it doesn't matter
+//just dont mix single and double quotes 
