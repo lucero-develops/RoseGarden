5291ed160eb7ea96f6352e599b05c83590e59499
lucero-develops <mike.j.lucero@gmail.com>
2017-01-23 13:14:45 -0500

    fizzbuzz 2.0

--- Hush.js
+++ Hush.js
@@ -1,7 +1,25 @@
-//prints numbers 1-10 to the console
+/*
+Print all numbers from 1 to 100 with three exceptions:
 
-for(var i = 0; i < 11; i++){
-	console.log(i);
+	-if the number is divisible by 3, print fizz
+	-if the number is divisible by 5, print buzz
+	-if the number is divisible by 3 AND 5 , print FizzBuzz
+
+*/
+
+for(var i=1; i<=100; i++){
+	if(i%3 === 0){
+		if(i%5 === 0){
+			console.log("FizzBuzz");
+		}
+		else{
+			console.log("fizz");
+		}
+	}
+	else if(i%5 ===0){
+		console.log("buzz");
+	}
+	else {
+		console.log(i);
+	}
 }
-
-//
