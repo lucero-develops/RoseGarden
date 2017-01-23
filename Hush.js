47493d7ee407cd0ea9efeaa8d93a6b2e6c2728ed
lucero-develops <mike.j.lucero@gmail.com>
2017-01-23 17:01:13 -0500

    avoid repetition with an  Object Constructor

--- Hush.js
+++ Hush.js
@@ -1,35 +1,43 @@
 /*
-Methods
-when an object has a property with a 
-function as the value, it is referred to 
-as a METHOD of that object
-*/
+What if we want to create multiple instances of an object?
 
+We can create something called an "object constructor",
+in which we use a function to create multiple
+instances of an object.
 
-var myCoffee = {
-	flavor: "black",
-	temperature: "hot",
-	ounces: 8,
-	milk: false,
+Each instance inherits the properties and methods
+of its constructor
 
-	reheat: function(){
-		if(myCoffee.temperature === "cold"{
-			myCoffee.temperature = "hot";
-			alert("Your coffee has been reheated");
-		}
-	}
-	// you leave off the  on the last value
-	//because the trailing comma causes an error on
-	//older browsers
+ */
+
+var steve = {
+	name: "Steve";
+	tshirtColor: "green"
 };
 
-myCoffee.temperature = "cold";
-// myCoffee["temperature"] = "lukewarm";
-// another way to assign a value besides dot notation
+var mike = {
+	name: "Mike";
+	tshirtColor: "red"
+};
 
-myCoffee.reheat();
+function Friend(name, tshirtColor) {
+	this.name = name;
+	this.tshirtColor = tshirtColor;
 
-//
+}
+
+var steve = new Friend("Steve", "green");
+var mike = new Friend("Mike", "red");
+
+alert(steve.name);
+//"Steve"
+
+// You get the same result as our object literal but
+// but we now can use the "Friend" object as many 
+//times as we want and avoid any kind of repetition in our code 
+
+
+
 
 
 
