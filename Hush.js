77f36839bfefa6bba6a6df197e09981d7fb87c53
lucero-develops <mike.j.lucero@gmail.com>
2017-01-23 12:42:08 -0500

    switching things up ,1st switch statment

--- Hush.js
+++ Hush.js
@@ -1,24 +1,24 @@
-/*
-Let's create a text adventure game with JavaScript.
-Let's set a senario in which we encounter a zombie,
-choose a weapon to fight the zombie with,
-and have a 50/50 chance of ethier defeating the zombie with 
-our weapon or getting bitten and losing the game.
-*/
+//Switching things up 
 
-alert("This is a zombie attack, you for was searching " +
-	"for food but found a zombie !");
-var weapon = prompt("Look for a weapon ! There is " +
-	"a bow and arrow, an axe, or a rubber chicken?");
-var randomNumber = Math.round(Math.random());
+var sign = prompt("What is your atrological sign?");
 
-alert("you attacked the zombie with your" + "" + weapon);
+alert("sensing...sensing your future.");
 
-if(randomNumber ===0){
-	alert("The zombie bites you. You lose!!!");
-}
-else if(randomNumber===1){
-	alert("You killed the zombie with your" +
-		weapon + ". You Win!");
+switch(sign) {
+	case "Taurus":
+		alert("Good Fortune is near ");
+		break;
+	case "Virgo":
+		alert("Your emotions will make you feel things");
+		break;
+	case "Leo":
+		alert("Let the power of the sun charge you ");
+		break;
+	default:
+		alert("please enter a valid sign");
+		break;
+	
 }
 
+/*if you don't include a break; the next case will act as true 
+so its important to add your breaks
